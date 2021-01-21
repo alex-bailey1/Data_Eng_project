06bea38e0618d8b2f284cc593749680db5a8939e
alex-bailey1 <bail34@pdx.edu>
2021-01-20 16:21:51 -0800

    creating consumer.py

---
+++ consumer.py
@@ -0,0 +1,58 @@
+from confluent_kafka import Consumer
+import json
+import sys
+sys.path.append("/home/bail34/examples/clients/cloud/python/")
+import ccloud_lib
+
+if __name__ == '__main__':
+
+    # Read arguments and configurations and initialize
+    config_file = "/home/bail34/.confluence/librdkafka.config"
+    topic = "project_topic"
+    conf = ccloud_lib.read_ccloud_config(config_file)
+
+    # Create Consumer instance
+    # 'auto.offset.reset=earliest' to start reading from the beginning of the
+    #   topic if no committed offsets exist
+    consumer = Consumer({
+        'bootstrap.servers': conf['bootstrap.servers'],
+        'sasl.mechanisms': conf['sasl.mechanisms'],
+        'security.protocol': conf['security.protocol'],
+        'sasl.username': conf['sasl.username'],
+        'sasl.password': conf['sasl.password'],
+        'group.id': 'project_group',
+        'auto.offset.reset': 'earliest',
+    })
+
+    # Subscribe to topic
+    consumer.subscribe([topic])
+
+    # Process messages
+    # total_count = 0
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # No message available within timeout.
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting for message or event/error in poll()")
+                continue
+            elif msg.error():
+                print('error: {}'.format(msg.error()))
+            else:
+                # Check for Kafka message
+                record_key = msg.key()
+                record_value = msg.value()
+                data = json.loads(record_value)
+                count = data['count']
+                # total_count += count
+                # print("Consumed record with key {} and value {}, \
+                #       and updated total count to {}"
+                #       .format(record_key, record_value, total_count))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
--- gatherer.py
+++ gatherer.py
@@ -1,5 +1,5 @@
 # Python Data Gatherer - Python3
-# For Data Engineering Project Assignment1 Part B (and later, E)
+# For Data Engineering Project Assignment1 
 # 1/13/2021
 
 # Import
@@ -7,20 +7,16 @@ from datetime import datetime
 from urllib import request
 import json
 from confluent_kafka import Producer, KafkaError
-# import ccloud_lib
 import sys
 sys.path.append("/home/bail34/examples/clients/cloud/python/")
 import ccloud_lib
 
-# Get data and write it to a file
-# date = datetime.today().strftime("%Y-%m-%d")
-# with open(date + '.json', 'w') as output_file:
+# Get data
 request = request.urlopen('http://rbi.ddns.net/getBreadCrumbData')
 the_parse = json.load(request)
 
 
 # Read arguments and configurations and initialize
-# args = ccloud_lib.parse_args()
 config_file = "/home/bail34/.confluence/librdkafka.config"
 topic = "project_topic"
 conf = ccloud_lib.read_ccloud_config(config_file)
@@ -37,8 +33,6 @@ producer = Producer({
 # Create topic if needed
 ccloud_lib.create_topic(conf, topic)
 
-# delivered_records = 0
-
 # Optional per-message on_delivery handler (triggered by poll() or flush())
 # when a message has been successfully delivered or
 # permanently failed delivery (after retries).
@@ -49,19 +43,13 @@ def acked(err, msg):
     """
     if err is not None:
         print("Failed to deliver message: {}".format(err))
-    # else:
-        # print("successfully delivered")
-        # delivered_records += 1
-        # print("Produced record to topic {} partition [{}] @ offset {}"
-                # .format(msg.topic(), msg.partition(), msg.offset()))
+
 
 for record in the_parse:
     record_key = "bus_data"
     record_value = json.dumps(record)
-    # print("Producing record: {}\t{}".fodrmat(record_key, record_value))
     producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
-    # p.poll() serves delivery reports (on_delivery)
-    # from previous produce() calls.
+    
     producer.poll(0)
     producer.flush()
 
@@ -80,6 +68,6 @@ print("finished")
 # https://stackoverflow.com/questions/606191/convert-bytes-to-a-string
 # https://docs.python.org/3/library/http.client.html
 # https://docs.python.org/3/library/urllib.request.html#module-urllib.request
-# https://github.com/confluentinc/examples/blob/6.0.1-post/clients/cloud/python/producer.py
+# producer code: https://github.com/confluentinc/examples/blob/6.0.1-post/clients/cloud/python/producer.py
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/
 # https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
