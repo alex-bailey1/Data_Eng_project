e5b36aaca1e163e21e41f8ca686f67ff951392fd
root <root@consumer.c.data-engineering-w2021-jcrisan.internal>
2021-02-23 00:38:45 +0000

    updates

--- geojson_convert.py
+++ geojson_convert.py
@@ -4,8 +4,8 @@ from geojson import Feature, FeatureCollection, Point
 import psycopg2
 
 DBname = "postgres"
-DBuser = ""
-DBpwd = ""
+DBuser = "database1"
+DBpwd = "database1?"
 
 def write_from_db():
     connection = psycopg2.connect(
@@ -17,8 +17,6 @@ def write_from_db():
     connection.autocommit = True
     with connection.cursor() as cursor:
         cursor.execute("""SELECT latitude, longitude, speed FROM BreadCrumb LIMIT 10;""")
-        # print("data:")
-        # print(cursor.fetchall())
         return cursor.fetchall()
 
 
@@ -26,7 +24,6 @@ def write_from_db():
 def convert_to_geojson(data):
     features = []
     for line in data:        
-        # Uncomment these lines
         lat = line[0]
         long_val = line[1]
         speed = line[2]
@@ -37,8 +34,6 @@ def convert_to_geojson(data):
      	
         try:
             latitude, longitude = map(float, (lat, long_val))
-            #print(latitude)
-            #print(longitude)
             features.append(
                 Feature(
                     geometry = Point((longitude,latitude)),
@@ -55,42 +50,9 @@ def convert_to_geojson(data):
 
 
 def write_to_file(features):
-    with open("data.geojson", "w")as wr_file:
-        json.dumps(features, wr_file)
-
-# features = []
-# with open('sample_data.tsv', newline='') as csvfile:
-#     reader = csv.reader(csvfile, delimiter='\t')
-#     data = csvfile.readlines()
-#     for line in data[1:]:
-#         row = line.split("\t")
-        
-#         # Uncomment these lines
-#         # lat = row[Index of latitude column]
-#         # long = row[Index of longitude column]
-#         # speed = row[Index of velocity column]
-
-#         # skip the rows where speed is missing
-#         if speed is None or speed == "":
-#             continue
-     	
-#         try:
-#             latitude, longitude = map(float, (lat, long))
-#             features.append(
-#                 Feature(
-#                     geometry = Point((longitude,latitude)),
-#                     properties = {
-#                         'speed': (int(speed))
-#                     }
-#                 )
-#             )
-#         except ValueError:
-#             continue
-
-# collection = FeatureCollection(features)
-# with open("data.geojson", "w") as f:
-#     f.write('%s' % collection)
-
+    collection = FeatureCollection(features)
+    with open("data.geojson", "w") as f:
+        f.write('%s' % collection)
 
 data = write_from_db()
 
